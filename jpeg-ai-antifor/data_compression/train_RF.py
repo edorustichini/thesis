5b911f369cbb0dcb6d72afd16a316f1188d5678f
Edoardo Rustichini <edorusti@gmail.com>
2025-07-13 11:53:38 +0200

    On the fly training

--- jpeg-ai-antifor/data_compression/train_RF.py
+++ jpeg-ai-antifor/data_compression/train_RF.py
@@ -6,116 +6,177 @@ import argparse
 
 from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
 from sklearn.utils import shuffle
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split, GridSearchCV
 
 from tqdm import tqdm
 import pickle
+import joblib
+from jpegai_compress_directory import * #setup_coder
+
+from PIL import Image
 
 def get_name(path: str) -> str:
     return os.path.splitext(os.path.basename(path))[0]
 
-def create_dataset(dataset,latents_dir, target):
+def get_latents(img_path, coder, target='y'):
+    decisions = coder.get_latents(img_path, bin_path=None, dec_save_path=None)
+
+    latent = decisions['CCS_SGMM']
+
+    features = {
+            'model_y': latent['model_y'][target],
+            'model_uv' :latent['model_uv'][target]
+            }
     
-    latents_set = []
+    return features
+
+
+
+def process_image(path):
+    img_path = path
+    ext = os.path.splitext(img_path)[1].lower()
+
+    if ext != ".png":
+        # Crea path temporaneo in .png
+        img_path_png = os.path.splitext(img_path)[0] + ".png"
+
+        # Converti in PNG
+        with Image.open(img_path) as im:
+            im.convert("RGB").save(img_path_png)
+
+        img_path = img_path_png  # usa il path PNG
+
+        # temporary_files.append(img_path_png)
+
+    return img_path
+
+
+def create_dataset(coder, dataset,img_dir, target='y'):
+
+    features_set = []
     labels = []
-
-    dataset = dataset.set_index("id")
-
-    for dir in glob.glob(latents_dir):
-        # Process the images
-        for file_path in tqdm(glob.glob(os.path.join(dir, "**","*.pt"), recursive=True)):
-
-            img_id = get_name(file_path)
-            #print(f"Adding {img_id} to the set")
-            
-
-            label = dataset.loc[img_id, "label"]
-            if label is None:
-                continue
-                #TODO: controllare se vuote ecc
-
-            #TODO: forse fare un try
-            data = torch.load(file_path, weights_only=False)
-            
-            latent = data[target]
-            latent_tensor = torch.cat([torch.flatten(latent['model_y']), torch.flatten(latent['model_uv'])]).cpu().numpy()
-            latents_set.append(latent_tensor)
-            labels.append(label)
-
-            
-    latents_array = np.array(latents_set)
-    labels_array = np.array(labels)
-    latents_shuffled, labels_shuffled = shuffle(latents_array, labels_array, random_state=42) #TODO: decidere random_state
     
-    return latents_shuffled, labels_shuffled
+    temp_file = []
+
+    for idx, row in tqdm(dataset.iterrows(), total=len(dataset)):
+    
+        img_path = os.path.join(img_dir, str(row['path']))
+        ext = os.path.splitext(img_path)[1].lower()
+
+        if ext != ".png":
+            # Crea path temporaneo in .png
+            img_path_png = os.path.splitext(img_path)[0] + ".png"
+
+            # Converti in PNG
+            with Image.open(img_path) as im:
+                im.convert("RGB").save(img_path_png)
+            
+            img_path = img_path_png  # usa il path PNG
+            temp_file.append(img_path)
+        
+        features = get_latents(img_path, coder)
+        features = torch.cat([torch.flatten(features['model_y']), torch.flatten(features['model_uv'])]).cpu().numpy() #TODO controllare .cpu, .numpy()
+        features_set.append(features)
+        
+        label = row['label']
+        labels.append(label)
+    
+    for path in temp_file:
+        if os.path.exists(path):
+            os.remove(path)
+
+
+    features_array = np.array(features_set)
+    labels_array = np.array(labels)
+    return shuffle(features_array, labels_array, random_state=42) #TODO: decidere se 
 
 def save_model(model_obj, save_dir, model_name):
-    model_pkl_file = os.path.join(save_dir, model_name+".pkl")
-    with open(model_pkl_file, 'wb') as file:  
-        pickle.dump(model_obj, file)   
+    os.makedirs(save_dir, exist_ok=True) 
+    model_path = os.path.join(save_dir, model_name + ".joblib")
+    joblib.dump(model_obj, model_path)
 
-def load_model(model_pkl_file):
-    with open(model_pkl_file, 'rb') as file:  
-        model = pickle.load(model_pkl_file)
-    return model 
+def load_model(model_path):
+    model = joblib.load(model_path)
+    return model
+
+def sample(df, N):
+    df_1 = df[df['label']==1].sample(N//2)
+    df_0 = df[df['label']==0].sample(N//2)
+    df = pd.concat([df_1, df_0])
+    return df
+
+def setup(args):
+    # --- Setup the device --- #
+    if args.gpu is not None:
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
+        print(f"Using GPU {args.gpu}")
+        args.target_device = 'gpu'
+
+    # --- Setup the coder --- #
+    encoder_parser = create_custom_parser(args)
+
+    # --- Setup the encoder --- #
+    coder = RecoEncoder(encoder_parser, def_encoder_parser_decorator(encoder_parser))
+
+
+    # --- Setup the coding engine --- #
+    coder.print_coder_info()
+
+    kwargs, params, _ = coder.init_common_codec(build_model=True, ce=None, cmd_args = None,
+                                                overload_ce=True, cmd_args_add=False)
+    profiler_path = kwargs.get('profiler_path', None)
+    # print(params)
+
+    # Load the models
+    coder.load_models(get_downloader(kwargs.get('models_dir_name', 'models'), critical_for_file_absence=not kwargs.get('skip_loading_error', False)))
+    coder.set_target_bpp_idx(kwargs['bpp_idx'])
+    return coder
 
 
 if __name__=="__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("train_dir", help="Path to train' data's latents")
-    parser.add_argument("train_csv", help="Path to dataset's csv file")
-
-    parser.add_argument("test_dir", help="Path to train' data's latents")
-    parser.add_argument("test_csv", help="Path to test's csv file")
-
+    # --- Setup an argument parser --- #
+    # Arguments for coder
+    parser = argparse.ArgumentParser(description='Compress a directory of images using the RecoEncoder')
+    parser.add_argument('--gpu', type=int, default=None, help='GPU index')
+    parser.add_argument('--imgs_path', type=str, default='../../real_vs_fake/real-vs-fake', help='Input directory')
+    parser.add_argument('input_path', type=str, default='../../input_imgs', help='Input directory')
+    parser.add_argument('bin_path', type=str, default='../../JPEGAI_output/', help='Save directory')
+    parser.add_argument('--set_target_bpp', type=int, default=1, help='Set the target bpp '
+                                                                      '(multiplied by 100)')
+    parser.add_argument('--models_dir_name', type=str, default='../../jpeg-ai-reference-software/models', help='Directory name for the '
+                                                                                 'models used in the encoder-decoder'
+                                                                                 'pipeline')
+    #Arguments for training
+    parser.add_argument('--num_samples', type=int, default=10, help='Number of samples to process')
+    parser.add_argument("--train_csv",default="../../train.csv" , help="Path to dataset's csv file")
+    parser.add_argument("--test_csv", default="../../test.csv", help="Path to test's csv file")
     parser.add_argument("-t", "--target",default="y", help="y_hat if quantized latent, else y")
+    parser.add_argument("--models_save_dir", default="/data/lesc/users/rustichini/thesis/models_saved", help="Directory to save models")
+    args = parser.parse_args()
 
-    args=parser.parse_args()
-    
+    coder = setup(args)
 
-    train_dir = args.train_dir
-    df_train = pd.read_csv(args.train_csv)
-    df_train = df_train.drop(columns = ["original_path", "path"])
-    
- 
-
-    test_dir =  args.test_dir
-    df_test = pd.read_csv(args.test_csv)
-    df_test = df_test.drop(columns = ["original_path", "path"])
-
+    imgs_dir = args.imgs_path
     target = args.target #TODO: da provare anche con hat
-
-    X_train, y_train = create_dataset(df_train, train_dir, target)
-    X_test, y_test = create_dataset(df_test, test_dir, target)
     
-    print("(num_samples, num_features)")
-    print(f"Train dataset : {X_train.shape}")
-    print(f"Test dataset: {X_test.shape}")
+    df_train = pd.read_csv(args.train_csv)
+    n_train_samples = args.num_samples
+    df_train = sample(df_train,n_train_samples)
+    X_train, y_train = create_dataset(coder, df_train,imgs_dir, target)
     
-
-    with open("/data/lesc/users/rustichini/thesis/models_saved/medium_dataset/y/simple_RF_250.pkl", "rb") as file:
-        model = pickle.load(file)
-    y_pred_rf = model.predict(X_test)
-
-    accuracy_rf = accuracy_score(y_test, y_pred_rf)
-    print(f"1. Accuracy on test set using simple RF: {accuracy_rf:.4f}")
-
+    #print("(num_samples, num_features)")
+    #print(f"Train dataset : {X_train.shape}")
     
-    '''
-    print("\nTraining simple random forest...")
-    rf = RandomForestClassifier(n_estimators=250, n_jobs=-1, random_state=42)
+    save_path = os.path.join(args.models_save_dir,str(n_train_samples) + "_samples",str(target))
+    print("Models will be save into "+ save_path)
+
+    print("\nTraining  random forest...")
+    rf = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=42)
     rf.fit(X_train, y_train)
     print("Training finished ")
     print("-"*90)
-    save_model(rf,"/data/lesc/users/rustichini/thesis/models_saved/medium_dataset/y", "simple_RF_250")
-
-    print("\nTraining HGB...")
-    hgb = HistGradientBoostingClassifier(random_state=42)
-    hgb.fit(X_train, y_train)
-    print("Training finished ")
-    print("-"*90)
-    save_model(hgb, "/data/lesc/users/rustichini/thesis/models_saved/medium_dataset/y", "hgb")
+    save_model(rf, save_path, model_name="RF_"+str(rf.n_estimators)+"_estimators")
     
     print("\nSearching with GridSearchCV...")
     param_grid = {
@@ -130,5 +191,5 @@ if __name__=="__main__":
     grid_search.fit(X_train,y_train)
     print("Training finished ")
     print("-"*90)
-    save_model(grid_search, "/data/lesc/users/rustichini/thesis/models_saved/medium_dataset/y", "grid_search_RF")
-    '''
+    save_model(grid_search, save_path, model_name="grid_search_RF") 
+    
