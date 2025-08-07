c61d5fcf74638fddf15fb60611ed62316772df83
Edoardo Rustichini <edorusti@gmail.com>
2025-08-07 15:21:38 +0200

    tsne

--- jpeg-ai-antifor/data_compression/visualization.py
+++ jpeg-ai-antifor/data_compression/visualization.py
@@ -12,8 +12,29 @@ def visualize(X, y, alg, reduce: bool=False):
     
 
 def main():
-    dataset_path = "/data/lesc/users/rustichini/thesis/models_saved/6_bpp/15000_samples/y/dataset.joblib"
-    X,y = load(dataset_path)
+    parser = argparse.ArgumentParser(description='Compress a directory of images using the RecoEncoder')
+    parser.add_argument('--gpu', type=int, default=0, help='GPU index')
+    parser.add_argument('--imgs_path', type=str, default='../../real_vs_fake/real-vs-fake', help='Input directory')
+    parser.add_argument('input_path', type=str, default='../../input_imgs', help='Input directory')
+    parser.add_argument('bin_path', type=str, default='../../JPEGAI_output/', help='Save directory')
+    parser.add_argument('--set_target_bpp', type=int, default=1, help='Set the target bpp '
+                                                                      '(multiplied by 100)')
+    parser.add_argument('--models_dir_name', type=str, default='../../jpeg-ai-reference-software/models', help='Directory name for the '
+                                                                                 'models used in the encoder-decoder'
+                                                                                 'pipeline')
+    #Arguments for training
+    parser.add_argument('--num_samples', type=int, default=1000, help='Number of samples to train on')
+    parser.add_argument('--num_samples_test', type=int, default=300, help='Number of samples to test on')
+    parser.add_argument('--random_sample', type=bool, default=False, help='Sample')
+    parser.add_argument("--train_csv",default="../../train.csv" , help="Path to dataset's csv file")
+    parser.add_argument("--test_csv", default="../../test.csv", help="Path to test's csv file")
+    parser.add_argument("-t", "--target",default=None, help="y_hat if quantized latent, else y")
+    parser.add_argument("--save", default=False, help="True if wanted to save dataset")
+    parser.add_argument("--models_save_dir", default="/data/lesc/users/rustichini/thesis/models_saved", help="Directory to save models")
+
+    args = parser.parse_args()
+    
+    X, X_hat, y = create_dataset()
     print("Dataset loaded")
     #print("PCA begins")
     #print("PCA finished")
